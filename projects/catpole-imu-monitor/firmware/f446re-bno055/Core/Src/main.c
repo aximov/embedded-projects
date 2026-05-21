ea5bc6bc8b6b2ba1eb46a6f61cc2ed32a3534e5e
Nakayama Daichi <d@alumni.tus.ac.jp>
2026-05-21 09:01:04 +0900

    format: main.c

--- projects/catpole-imu-monitor/firmware/f446re-bno055/Core/Src/main.c
+++ projects/catpole-imu-monitor/firmware/f446re-bno055/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -45,18 +45,18 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-#define BNO055_I2C_ADDR          (0x28 << 1)
+#define BNO055_I2C_ADDR (0x28 << 1)
 
-#define BNO055_CHIP_ID           0x00
-#define BNO055_PAGE_ID           0x07
-#define BNO055_ACCEL_DATA_X_LSB  0x08
-#define BNO055_OPR_MODE          0x3D
+#define BNO055_CHIP_ID 0x00
+#define BNO055_PAGE_ID 0x07
+#define BNO055_ACCEL_DATA_X_LSB 0x08
+#define BNO055_OPR_MODE 0x3D
 
-#define BNO055_MODE_CONFIG       0x00
-#define BNO055_MODE_ACCONLY      0x01
+#define BNO055_MODE_CONFIG 0x00
+#define BNO055_MODE_ACCONLY 0x01
 
-#define BNO055_ACC_CONFIG        0x08
-#define BNO055_ACC_CONFIG_VALUE  0x18
+#define BNO055_ACC_CONFIG 0x08
+#define BNO055_ACC_CONFIG_VALUE 0x18
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -80,32 +80,32 @@ uint8_t bno055_addr = 0;
 
 void I2C_Scan(void)
 {
-	i2c_found_count = 0;
-	bno055_found = 0;
-	bno055_addr = 0;
+  i2c_found_count = 0;
+  bno055_found = 0;
+  bno055_addr = 0;
 
-	for (uint8_t addr = 1; addr < 128; addr++)
-	{
-		if (HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(addr << 1), 2, 10) == HAL_OK)
-		{
-			i2c_found_addr[i2c_found_count] = addr;
-			i2c_found_count++;
+  for (uint8_t addr = 1; addr < 128; addr++)
+  {
+    if (HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(addr << 1), 2, 10) == HAL_OK)
+    {
+      i2c_found_addr[i2c_found_count] = addr;
+      i2c_found_count++;
 
-			if (addr == 0x28 || addr == 0x29)
-			{
-				bno055_found = 1;
-				bno055_addr = addr;
-			}
-		}
-	}
+      if (addr == 0x28 || addr == 0x29)
+      {
+        bno055_found = 1;
+        bno055_addr = addr;
+      }
+    }
+  }
 }
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -138,11 +138,11 @@ int main(void)
 
   if (bno055_init_for_accel() == HAL_OK)
   {
-      printf("BNO055 init OK\r\n");
+    printf("BNO055 init OK\r\n");
   }
   else
   {
-      printf("BNO055 init ERR\r\n");
+    printf("BNO055 init ERR\r\n");
   }
 
   uint8_t chip_id = 0;
@@ -167,63 +167,64 @@ int main(void)
 
   while (1)
   {
-	uint32_t now_ms = HAL_GetTick();
-	uint32_t elapsed_ms = now_ms - start_ms;
+    uint32_t now_ms = HAL_GetTick();
+    uint32_t elapsed_ms = now_ms - start_ms;
 
-	if ((now_ms - last_ms) >= 100)
-	{
-		last_ms = now_ms;
+    if ((now_ms - last_ms) >= 100)
+    {
+      last_ms = now_ms;
 
-		int16_t ax = 0;
-		int16_t ay = 0;
-		int16_t az = 0;
+      int16_t ax = 0;
+      int16_t ay = 0;
+      int16_t az = 0;
 
-		if (bno055_read_accel(&ax, &ay, &az) == HAL_OK)
-		{
-			printf("%lu,%d,%d,%d\r\n",
-				   elapsed_ms,
-				   ax,
-				   ay,
-				   az);
-		}
-		else
-		{
-			printf("%lu,ERR,ERR,ERR\r\n", elapsed_ms);
-		}
-	}
+      if (bno055_read_accel(&ax, &ay, &az) == HAL_OK)
+      {
+        printf("%lu,%d,%d,%d\r\n",
+               elapsed_ms,
+               ax,
+               ay,
+               az);
+      }
+      else
+      {
+        printf("%lu,ERR,ERR,ERR\r\n", elapsed_ms);
+      }
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	if (bno055_found) {
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-		HAL_Delay(500); // found. blink slowly
-	}
-	else
-	{
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-		HAL_Delay(100); // not found: blink fast
-	}
+    if (bno055_found)
+    {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+      HAL_Delay(500); // found. blink slowly
+    }
+    else
+    {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+      HAL_Delay(100); // not found: blink fast
+    }
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -240,9 +241,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -255,10 +255,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C1_Init(void)
 {
 
@@ -285,14 +285,13 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -318,14 +317,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -461,54 +459,52 @@ HAL_StatusTypeDef bno055_init_for_accel(void)
 
 HAL_StatusTypeDef bno055_read_accel(int16_t *ax, int16_t *ay, int16_t *az)
 {
-	uint8_t buf[6];
+  uint8_t buf[6];
 
-	HAL_StatusTypeDef status = HAL_I2C_Mem_Read(
-		&hi2c1,
-		BNO055_I2C_ADDR,
-		BNO055_ACCEL_DATA_X_LSB,
-		I2C_MEMADD_SIZE_8BIT,
-		buf,
-		6,
-		100
-	);
+  HAL_StatusTypeDef status = HAL_I2C_Mem_Read(
+      &hi2c1,
+      BNO055_I2C_ADDR,
+      BNO055_ACCEL_DATA_X_LSB,
+      I2C_MEMADD_SIZE_8BIT,
+      buf,
+      6,
+      100);
 
-	if (status != HAL_OK)
-	{
-		return status;
-	}
+  if (status != HAL_OK)
+  {
+    return status;
+  }
 
-	*ax = (int16_t)((buf[1] << 8) | buf[0]);
-	*ay = (int16_t)((buf[3] << 8) | buf[2]);
-	*az = (int16_t)((buf[5] << 8) | buf[4]);
+  *ax = (int16_t)((buf[1] << 8) | buf[0]);
+  *ay = (int16_t)((buf[3] << 8) | buf[2]);
+  *az = (int16_t)((buf[5] << 8) | buf[4]);
 
-	return HAL_OK;
+  return HAL_OK;
 }
 
 HAL_StatusTypeDef bno055_read_u8(uint8_t reg, uint8_t *value)
 {
-    return HAL_I2C_Mem_Read(
-        &hi2c1,
-        BNO055_I2C_ADDR,
-        reg,
-        I2C_MEMADD_SIZE_8BIT,
-        value,
-        1,
-        100
-    );
+  return HAL_I2C_Mem_Read(
+      &hi2c1,
+      BNO055_I2C_ADDR,
+      reg,
+      I2C_MEMADD_SIZE_8BIT,
+      value,
+      1,
+      100);
 }
 
 int _write(int file, char *ptr, int len)
 {
-    HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
-    return len;
+  HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+  return len;
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -521,12 +517,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
